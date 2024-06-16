a611f2560bed73e3153e467a2a3a8944eb8e1098
Thanh Kieu <thanhkieu.tttk1982003@gmail.com>
2024-06-17 01:16:52 +0700

    Update ThuongHieuController.cs

--- FashionShop/Areas/Admin/Controllers/ThuongHieuController.cs
+++ FashionShop/Areas/Admin/Controllers/ThuongHieuController.cs
@@ -22,7 +22,6 @@ namespace FashionShop.Areas.Admin.Controllers
         [HttpGet]
         public async Task<IActionResult> Index(string sortOrder, string currentFilter, string searchString, int? pageNumber)
         {
-            ViewData["CurrentSort"] = sortOrder;
             ViewData["NameSortParm"] = sortOrder == "Name" ? "name_desc" : "Name";
             ViewData["StatusSortParm"] = sortOrder == "Status" ? "status_desc" : "Status";
 
